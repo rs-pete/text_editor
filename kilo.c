7ade48f1298a25c9d86272f6660bbbf78b319272
rs-pete <rohitsebastianp@gmail.com>
2024-08-24 22:07:05 +0200

    added timeout for read
    shows 0 for in 1/10 sec if no input is given

--- kilo.c
+++ kilo.c
@@ -22,8 +22,8 @@ void enableRawMode() {
     raw.c_cflag |= (CS8);
     raw.c_oflag &= ~(OPOST);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN); //removes echoing chars before "q" und icanon = see tings we type
-    //raw.c_cc[VMIN] = 0;
-    //raw.c_cc[VTIME] = 1;
+    raw.c_cc[VMIN] = 0; //min bytes before return
+    raw.c_cc[VTIME] = 1; //max wait time for read; here 1/10 sec
     
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -31,14 +31,16 @@ void enableRawMode() {
 int main() {
     enableRawMode();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {   //reads 1 char from stdin
+    while (1) {
+        char c = '\0';
+        read(STDIN_FILENO , &c , 1);
         if (iscntrl(c)) { //cortol char chk
             printf("%d\r\n", c);
         }
         else {
             printf("%d ('%c')\r\n", c, c); // 98('a')
         }
+        if( c == 'q') break;
     }
     return 0;
 }
