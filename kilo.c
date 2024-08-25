4a01c79019c1d0b4f2ce5680711837d400986c44
rs-pete <rohitsebastianp@gmail.com>
2024-08-25 15:08:03 +0200

    added proper fallback
    fallback test case still present

--- kilo.c
+++ kilo.c
@@ -58,6 +58,25 @@ char editorReadKey() {
     return c;
 }
 
+int getCursorPosition( int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n" , 4 ) != 4) return -1;
+
+    while ( i < sizeof( buf ) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
+    }
+    
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;
+}
 int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
 
@@ -66,8 +85,7 @@ int getWindowSize(int *rows, int *cols) {
     if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) { 
         //fallbak if iocntl wont work
         if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
-            editorReadKey();
-        return -1;
+        return getCursorPosition(rows, cols);
     } 
     else {
         //terminal widith and height
