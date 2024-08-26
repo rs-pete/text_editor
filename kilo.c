fe3685794c73fdffe12e28a18caa9c06ffcc9116
rs-pete <rohitsebastianp@gmail.com>
2024-08-26 20:27:55 +0200

    movement with wasd
    movement error in previous commit

--- kilo.c
+++ kilo.c
@@ -163,7 +163,6 @@ void editorRefreshScreen() {
     snprintf(buf , sizeof(buf) , "\x1b[%d;%dH" , E.cy + 1 , E.cx + 1);
     abAppend(&ab, buf, strlen(buf));
 
-    abAppend( &ab , "\x1b[H" , 3 );
     abAppend( &ab , "\x1b[?25h" , 6 ); //toggle cursor
 
     write( STDOUT_FILENO, ab.b , ab.len );
@@ -171,6 +170,24 @@ void editorRefreshScreen() {
 }
 
 /**** input ****/
+
+void editorMoveCursor(char key) {
+  switch (key) {
+    case 'a':
+      E.cx--;
+      break;
+    case 'd':
+      E.cx++;
+      break;
+    case 'w':
+      E.cy--;
+      break;
+    case 's':
+      E.cy++;
+      break;
+  }
+}
+
 
 void editorProcessKeypress() {
     char c = editorReadKey();
@@ -181,6 +198,12 @@ void editorProcessKeypress() {
             write(STDOUT_FILENO, "\x1b[H", 3 );
             exit(0);
             break;
+            case 'w':
+            case 's':
+            case 'a':
+            case 'd':
+                editorMoveCursor(c);
+                break;
     }
 }
 
